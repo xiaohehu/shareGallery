90f1dae74a553a5d4ef3c5c3d6a1715aaad5aa39
xiaohehu <xiaohe.hu@27_imac.neoscape.local>
2015-02-05 17:27:21 -0500

    first commit

---
+++ shareGallery/galleryCell.m
@@ -0,0 +1,13 @@
+//
+//  galleryCell.m
+//  shareGallery
+//
+//  Created by Xiaohe Hu on 2/5/15.
+//  Copyright (c) 2015 Neoscape. All rights reserved.
+//
+
+#import "galleryCell.h"
+
+@implementation galleryCell
+
+@end
